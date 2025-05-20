36a8786e16089b6f52c4cd27f03a6cc22ca3d426
joao1824 <joao.1824@alunosatc.edu.br>
2025-05-20 09:42:11 -0300

    .

--- Projeto/src/app/app.component.ts
+++ Projeto/src/app/app.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Projeto';
+  novaMensagem: string = '';
+  mensagens: { autor: 'voce' | 'atendente', texto: string }[] = [
+    { autor: 'atendente', texto: 'Olá, em que posso ajudar?' }
+  ];
+
+  enviarMensagem(event?: Event) {
+    event?.preventDefault();
+
+    if (this.novaMensagem.trim()) {
+      this.mensagens.push({
+        autor: 'voce',
+        texto: this.novaMensagem.trim()
+      });
+      this.novaMensagem = '';
+      setTimeout(() => {
+        const chat = document.querySelector('.conteudo_chat');
+        if (chat) chat.scrollTop = chat.scrollHeight;
+      });
+    }
+  }
 }
